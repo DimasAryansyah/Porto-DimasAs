fb4814773693223051f90a51f392c855d0ffec50
Dimas <dimasaryansyah02560@gmail.com>
2025-10-04 00:12:50 +0700

    upload portofolio

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle menu dan ikon hamburger
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active"); // ubah hamburger jadi X
+  navLinks.classList.toggle("show"); // tampilkan / sembunyikan menu
+});
